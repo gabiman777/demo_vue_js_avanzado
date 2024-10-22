15bbcb7bd7ea537dd204d5e5cdb540dbf0fcb95d
gperez <gperez@nsa.com.py>
2024-10-22 14:24:56 -0300

    módulos

---
+++ js/app.js
@@ -0,0 +1,15 @@
+import {sandia, pintarPlatano, guinda} from "./frutas.js"
+
+console.log(sandia)
+pintarPlatano()
+
+guinda()
+
+/*
+(
+    function (){
+        const fruta = "🍉"
+        console.log(fruta)
+    }
+)
+*/
---
+++ js/frutas.js
@@ -0,0 +1,27 @@
+export const sandia = "🍉"
+export const limon = "🍋"
+
+export function pintarPlatano(){
+    console.log("🍌")
+}
+
+export function guinda(){
+    console.log("🍒")
+}
+
+const frutilla = () => {
+    console.log("🍓");
+};
+
+export default function() {
+    console.log("🍒");
+}
+
+/*
+(
+    () => {
+        const fruta = "🍉"
+        console.log(fruta)
+    }
+)
+*/
